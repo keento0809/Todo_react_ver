c317cf7b13a180b4bb124533d4abcc1cf983c539
KEENTO <handball0321@gmail.com>
2022-02-13 12:14:56 -0800

    Add some css style to the list

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,6 +1,25 @@
 import React, { useState, useContext } from "react";
 import ListContext from "../../contexts/list-context";
 import AddItem from "../../actions/list-action";
+import styled from "styled-components";
+
+const ListDiv = styled.div`
+  list-style: none;
+  width: 90%;
+  margin: 0 auto;
+  border: 3px solid #888;
+
+  & input {
+    margin: 20px 2px 0 0;
+    border: 3px solid pink;
+  }
+`;
+
+const UlStyles = styled.ul`
+  width: 90%;
+  padding: 0;
+  margin: 0 auto;
+`;
 
 const List = () => {
   const listCtx = useContext(ListContext);
@@ -15,15 +34,15 @@ const List = () => {
   };
 
   return (
-    <div>
+    <ListDiv>
       <input type="text" onChange={changeTextValueHandler} />
       <button onClick={addItemHandler}>ADD</button>
-      <ul>
+      <UlStyles>
         {listCtx.state.items.map((item, index) => (
           <p key={index}>{item}</p>
         ))}
-      </ul>
-    </div>
+      </UlStyles>
+    </ListDiv>
   );
 };
 
