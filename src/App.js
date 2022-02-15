12ba0d455e827f6f3dd447150adf1dac9ac59a56
KEENTO <handball0321@gmail.com>
2022-02-15 15:24:09 -0800

    Add Task.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import React, { useState } from "react";
 import Main from "./components/Main/Main";
 import ListProvider from "./contexts/ListProvider";
+import Task from ".//components/Task/Task";
 import classes from "./App.module.css";
 
 const App = () => {
+  const [isModalShown, setIsModalShown] = useState(false);
+
   return (
     <ListProvider>
+      {isModalShown && <Task />}
       <div className={classes.app}>
         <Main />
       </div>
--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -4,7 +4,10 @@ import ListContext from "../../contexts/list-context";
 const Task = (props) => {
   const listCtx = useContext(ListContext);
 
-  const allTasks = listCtx.items.map((item) => <p>{item}</p>);
+  //   const allTasks = listCtx.items.map((item) => <p>{item}</p>);
+  //   const selectedItemIndex = state.items.findIndex((item) => item.id === )
 
   return <div>aaa</div>;
 };
+
+export default Task;
