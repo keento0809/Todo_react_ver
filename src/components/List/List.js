d9710596050fc9716a4af7d69e6a28b3ff3eacf1
KEENTO <handball0321@gmail.com>
2022-02-16 21:39:15 -0800

    Fixed delete function a little

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -19,8 +19,10 @@ const UlStyle = styled.ul`
 
 const List = (props) => {
   const listCtx = useContext(ListContext);
+  // state for textInput
   const [textValue, setTextValue] = useState("");
   const [filteredItems, setFilteredItems] = useState(listCtx.state.items);
+  // state for searchInput
   const [searchValue, setSearchValue] = useState("");
   const [textIsValid, setTextIsValid] = useState(true);
 
@@ -38,14 +40,14 @@ const List = (props) => {
   };
 
   useEffect(() => {
+    // filtering items first, then map the list
     setFilteredItems(
       listCtx.state.items.filter((item) => item.includes(searchValue))
     );
     console.log(filteredItems);
-  }, [textValue, searchValue]);
+  }, [listCtx.state.items, textValue, searchValue]);
 
   const searchChangeHandler = (e) => {
-    // setSearchTerm(e.target.value);
     console.log(e.target.value);
     setSearchValue(e.target.value);
   };
@@ -70,7 +72,6 @@ const List = (props) => {
   return (
     <div>
       <SearchInput onChange={searchChangeHandler} />
-      {/* <TaskInput /> */}
       <InputStyle>
         <div>
           <input
@@ -84,16 +85,6 @@ const List = (props) => {
         {!setTextIsValid && <p>Please enter a valid text (2-15) </p>}
       </InputStyle>
       <UlStyle>
-        {/* {listCtx.state.items.map((item, index) => (
-          <Card key={index} id={index} onClick={props.onOpen}>
-            <ListItem
-              id={index}
-              task={item}
-              onOpen={props.onOpen}
-              onRemove={removeItemHandler}
-            />
-          </Card>
-        ))} */}
         {filteredItems.map((item, index) => (
           <Card key={index} id={index} onClick={props.onOpen}>
             <ListItem
